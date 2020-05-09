cae5347e939e73beba619144594f0eaea5dcc9be
Nick Bernstein <skolyr@csu.fullerton.edu>
2020-05-08 23:44:25 -0700

    Changed range and step to be closer to first table.

--- chapter_1/1-4_cels.c
+++ chapter_1/1-4_cels.c
@@ -4,16 +4,16 @@
 
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table
-    for fahr = 0, 20, ..., 300; floating-point version */
+/* print Celsius-Fahrenheit table
+    for celsius = -20, -10, ..., 150; floating-point version */
 
 main(){
     float fahr, celsius;
     int lower, upper, step;
 
-    lower = 0;      /* lower limit of temperature table */
-    upper = 100;    /* upper limit */
-    step = 5;      /* step size */
+    lower = -20;      /* lower limit of temperature table */
+    upper = 150;    /* upper limit */
+    step = 10;      /* step size */
 
     /* heading */
     printf(" C  |   F\n");
