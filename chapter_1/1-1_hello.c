db5174ac0491ce83236c69ec5a7eff22d94b05d6
Nick Bernstein <skolyr@csu.fullerton.edu>
2020-05-08 22:55:39 -0700

    Completed exercise 1-1

---
+++ chapter_1/1-1_hello.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+// Good version
+int main(){
+    printf("hello, world.\n");
+}
+
+// // Missing semicolon
+// int main(){
+//     printf("hello, world.\n")
+// }
+// // 1-1_hello.c(11): error C2143: syntax error: missing ';' before '}'
+
+// // Missing closing parenthesis on printf()
+// int main(){
+//     printf("hello, world.\n";
+// }
+// // ERROR 1-1_hello.c(10): error C2143: syntax error: missing ')' before ';'
+
+// //  Missing closing bracket on main(){}
+// int main(){
+//     printf("hello, world.\n");
+// //  1-1_hello.c(16): fatal error C1075: '{': no matching token found
+
+
+// // Without <stdio.h>
+// int main(){
+//     printf("hello, world.\n");
+// }
+
+// // 1-1_hello.obj : error LNK2019: unresolved external symbol _printf referenced in function _main
+// // 1-1_hello.exe : fatal error LNK1120: 1 unresolved externals
