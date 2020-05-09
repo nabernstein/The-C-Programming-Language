ed368d2f10db20cb0e9f49adb6ec80ceee422268
Nick Bernstein <skolyr@csu.fullerton.edu>
2020-05-09 00:28:49 -0700

    Completed Exercise 1-7.

---
+++ chapter_1/1-7_eof.c
@@ -0,0 +1,16 @@
+/*
+    Exercise 1-7. Write a program to print the value of EOF.
+*/
+
+#include <stdio.h>
+
+main(){
+    printf("%d\n", EOF);
+}
+
+/* OUTPUT
+
+D:\The_C_Programming_Language_KandR\The-C-Programming-Language\chapter_1>1-7_eof.exe
+-1
+
+*/
